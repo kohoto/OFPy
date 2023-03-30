1af3956b530b3ab0d7801441da757c3df2cd42a0
Tohoko Tajima <tohoko.tj@tamu.edu>
2023-03-30 17:30:02 -0500

    changed prep batch to function to call from bash

--- OFPy_prep_batch.py
+++ OFPy_prep_batch.py
@@ -5,37 +5,38 @@ from OFPy_deform_mesh import prep_case
 dissolCases_directory = 'C:/Users/tohoko.tj/Documents/dissolCases_230329'
 # dissolCases_directory = '/scratch/user/tohoko.tj/dissolCases/dissolCases_230327/'
 start_proj_name = 'lambda2_0-2_0-stdev0_025'
-cases = ['etching', 'conductivity']
 
-# get polyMesh from etching folder.
-os.chdir(dissolCases_directory)
-dir_list = os.listdir(dissolCases_directory)
-dir_list.remove(start_proj_name)
 
-for case in cases:
-    os.system('chmod 775 ' + start_proj_name + '/' + case + '/PararellRun ' + start_proj_name + '/' + case + '/SingleRun ' + start_proj_name + '/' + case + '/Clean')
+def prep_batch(dissolCases_directory, start_proj_name):
+    cases = ['etching', 'conductivity']
+    # get polyMesh from etching folder.
+    os.chdir(dissolCases_directory)
+    dir_list = os.listdir(dissolCases_directory)
+    dir_list.remove(start_proj_name)
 
-# copy cases
-for new_proj_name in dir_list:
     for case in cases:
-        start_case_dir = start_proj_name + '/' + case
-        new_case_dir = new_proj_name + '/' + case
+        os.system('chmod 775 ' + start_proj_name + '/' + case + '/PararellRun ' + start_proj_name + '/' + case + '/SingleRun ' + start_proj_name + '/' + case + '/Clean')
 
-        cmd = ['mkdir ' + new_case_dir + '/constant;',
-               'cp -r ' + start_case_dir + '/constant ' + new_case_dir + ';',
-               'cp -r ' + start_case_dir + '/system ' + new_case_dir + ';',
-               'cp -r ' + start_case_dir + '/Zero ' + new_case_dir + ';',
-               'cp ' + start_case_dir + '/Clean ' + new_case_dir + ';',
-               'cp ' + start_case_dir + '/PararellRun ' + new_case_dir + ';',
-               'cp ' + start_case_dir + '/SingleRun ' + new_case_dir + ';',
-               'mkdir ' + new_case_dir + '/0;',
-               'chmod 775 ' + new_case_dir + '/PararellRun ' + new_case_dir + '/Clean']
-        os.system(''.join(cmd))
+    # copy cases
+    for new_proj_name in dir_list:
+        for case in cases:
+            start_case_dir = start_proj_name + '/' + case
+            new_case_dir = new_proj_name + '/' + case
+
+            cmd = ['mkdir ' + new_case_dir + '/constant;',
+                   'cp -r ' + start_case_dir + '/constant ' + new_case_dir + ';',
+                   'cp -r ' + start_case_dir + '/system ' + new_case_dir + ';',
+                   'cp -r ' + start_case_dir + '/Zero ' + new_case_dir + ';',
+                   'cp ' + start_case_dir + '/Clean ' + new_case_dir + ';',
+                   'cp ' + start_case_dir + '/PararellRun ' + new_case_dir + ';',
+                   'cp ' + start_case_dir + '/SingleRun ' + new_case_dir + ';',
+                   'mkdir ' + new_case_dir + '/0;',
+                   'chmod 775 ' + new_case_dir + '/PararellRun ' + new_case_dir + '/Clean']
+            os.system(''.join(cmd))
 
 
-# run case
-dir_list.append(start_proj_name)
-for proj in dir_list:
-    prep_case(proj, close=False)
-    prep_case(proj, close=True)
+    # create rough surface mesh
+    dir_list.append(start_proj_name)
+    for proj in dir_list:
+        prep_case(proj, close=False)
 
