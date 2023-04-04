fc32ff8feaf961b77709cbbe9a90d823bf64e54c
Tohoko Tajima <tohoko.tj@tamu.edu>
2023-04-04 16:53:59 -0500

    use symbolic link in prep_batch

--- OFPy_prep_batch.py
+++ OFPy_prep_batch.py
@@ -2,8 +2,8 @@ import os
 from . import OFPy_deform_mesh
 # For Linux and Windows (no OF commands), but prefered to run on Linux since chmod and need to transfer more files
 
-dissolCases_directory = 'C:/Users/tohoko.tj/Documents/dissolCases_230329'
-# dissolCases_directory = '/scratch/user/tohoko.tj/dissolCases/dissolCases_230327/'
+# dissolCases_directory = 'C:/Users/tohoko.tj/Documents/dissolCases_230329'
+dissolCases_directory = '/scratch/user/tohoko.tj/dissolCases/seed7500-stdev0_025/'
 start_proj_name = '/scratch/user/tohoko.tj/dissolCases/start_proj'
 
 
@@ -19,19 +19,33 @@ def prep_batch(dissolCases_directory, start_proj_name):
             start_case_dir = start_proj_name + '/' + case
             new_case_dir = new_proj_name + '/' + case
 
-            cmd = ['mkdir ' + new_case_dir + '/constant;',
-                   'cp -r ' + start_case_dir + '/constant/* ' + new_case_dir + '/constant;',
-                   'cp -r ' + start_case_dir + '/system ' + new_case_dir + ';',
-                   'cp -r ' + start_case_dir + '/Zero ' + new_case_dir + ';',
+            cmd = ['cp -rp ' + start_case_dir + '/constant ' + new_case_dir + '/constant;', # copy constant (cause polyMesh is unique)
+                   # remove constant except polyMesh
+                   'rm ' + new_case_dir + '/constant/dynamicMeshDict;',
+                   'rm ' + new_case_dir + '/constant/transportProperties;',
+                   'rm -rf' + new_case_dir + '/constant/bcInclude;',
+                   # add symbolic links in constant except polyMesh
+                   'ln -s ' + start_case_dir + '/constant/dynamicMeshDict ' + new_case_dir + '/constant/dynamicMeshDict;',
+                   'ln -s ' + start_case_dir + '/constant/transportProperties ' + new_case_dir + '/constant/transportProperties;',
+                   'ln -s ' + start_case_dir + '/constant/bcInclude ' + new_case_dir + '/constant/bcInclude;',
+                   # add symbolic links for other dirs
+                   'ln -s ' + start_case_dir + '/system ' + new_case_dir + ';',
+                   'ln -s ' + start_case_dir + '/Zero ' + new_case_dir + ';',
+                   # hard copy bash files
                    'cp ' + start_case_dir + '/Clean ' + new_case_dir + ';',
                    'cp ' + start_case_dir + '/PararellRun ' + new_case_dir + ';',
                    'cp ' + start_case_dir + '/SingleRun ' + new_case_dir + ';',
-                   'mkdir ' + new_case_dir + '/0;',
-                   'chmod 775 ' + new_case_dir + '/PararellRun ' + new_case_dir + '/Clean']
+                   # hard copy 0 folder (it will be rewritten by OF)
+                   'cp -rp ' + start_case_dir + '/Zero ' + new_case_dir + '/0;',
+                   'chmod 775 ' + new_case_dir + '/PararellRun ' + new_case_dir + '/SingleRun ' + new_case_dir + '/Clean']
             os.system(''.join(cmd))
 
 
     # create rough surface mesh
     for proj in dir_list:
         OFPy_deform_mesh.prep_case(proj, close=False)
+
+
+if __name__ == '__main__':
+    prep_batch(dissolCases_directory, start_proj_name)
 
