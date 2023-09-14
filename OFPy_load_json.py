051e1d0137b4e1c10e3b24b62a24e4de8c9dd5aa
tohoko.tj <msynyo@gmail.com>
2023-09-14 18:41:58 -0500

    updated load_json to load data from all batch in dissolCases

--- OFPy_load_json.py
+++ OFPy_load_json.py
@@ -3,6 +3,7 @@ import json
 import matplotlib.pyplot as plt
 import platform
 import numpy as np
+import pandas as pd
 
 if platform.system() == 'Windows':
     import plot_default_format
@@ -12,44 +13,77 @@ plt.style.use("my_style")
 
 # apply default format for plots
 plot_default_format.plot_format_Tohoko()
-def load_json_cond(dissolCases_directory):  # For Linux and Windows (no OF command)
+def load_json_cond(root_directory):  # For Linux and Windows (no OF command)
+    """
+
+    """
     initial_dir = os.getcwd()
     # store parameters
     par = {}
 
     # loop through each file in the directory
+    # correct folder names (not path)
+    dissolCases_directories = [name for name in os.listdir(root_directory) if os.path.isdir(root_directory + name)]
+    print(str(len(dissolCases_directories)) + ' batch detected.')
 
-    case_directories = [name for name in os.listdir(dissolCases_directory) if os.path.isdir(dissolCases_directory + name)]
-    print(str(len(case_directories)) + ' cases detected.')
-    for case_directory in case_directories:
-        filepath = dissolCases_directory + case_directory + '/cond.json'
-        with open(filepath, 'r') as f:
-            json_data = json.load(f)
-            # loop through each key-value pair in the json data
-            for key, value in json_data.items():
-                if key in par:
-                    par[key].append(value)  # add the value to the existing list
-                else:
-                    par[key] = [value]  # create a new list for the key
+    # seed and stdev fixed
+    dissolCases_directories = [root_directory + s for s in dissolCases_directories]
+    for dissolCases_directory in dissolCases_directories:
+        print('working on ' + dissolCases_directory + '...')
+        case_directories = [name for name in os.listdir(dissolCases_directory) if os.path.isdir(dissolCases_directory + '/' + name)]
+        print(str(len(case_directories)) + ' cases detected.')
 
-    key_x = 'stdev'
-    key_y = 'avg_w__in'
-    cubic_x = np.linspace(min(par[key_x]), max(par[key_x]), num=10)
-    # plt.loglog(cubic_x, cubic_x * cubic_x * cubic_x / 12.0 * 0.0254 * 1.0133e15 * 3.28, 'r-')
-    plt.loglog(par[key_x], par[key_y], 'o', label=dissolCases_directory.split("/")[-2])
-    save_plot(dissolCases_directory, key_x, key_y)
+        # lambdas changed for each case directory
+        for case_directory in case_directories:
+            filepath = dissolCases_directory + '/' + case_directory + '/cond.json'
+            with open(filepath, 'r') as f:
+                json_data = json.load(f)
+                # loop through each key-value pair in the json data
+                for key, value in json_data.items():
+                    if key in par:
+                        par[key].append(value)  # add the value to the existing list
+                    else:
+                        par[key] = [value]  # create a new list for the key
+
+    # convert to pandas
+    par = pd.DataFrame.from_dict(par)
+
+    # choose the formatting rules for the plots
+    # key_x = 'avg_w__in'; key_y = 'cond__mdft'; key_c = 'lambda_x__in'; key_m = 'stdev'
+    # key_x = 'stdev'; key_y = 'avg_w__in'; key_c = 'lambda_x__in'; key_m = 'lambda_x__in'
+    key_x = 'lambda_x__in'; key_y = 'avg_w__in'; key_c = 'stdev'; key_m = 'stdev'
+
+    iter = 0
+    marker = ['o', '^', 's', 'v']
+    fig, ax = plt.subplots()
+    grouped = par.groupby(key_m) # m for marker
+
+    for val_m in par[key_m].unique():
+        group = grouped.get_group(val_m)
+        im = ax.scatter(group[key_x], group[key_y], s=val_m * 1000, c=group[key_c], cmap='Greens', edgecolors='k')
+
+    save_plot(root_directory, ax, key_x, key_y, key_c, im)
 
     # go back to original directory
     os.chdir(initial_dir)
 
-def save_plot(dissolCases_directory, key_x, key_y):
-    plt.xlabel(key_x)
-    plt.ylabel(key_y)
-    plt.title(dissolCases_directory)
-    plt.legend()
-    # plt.show()
-    splitted_case_dir_path = dissolCases_directory.split("/")
-    imgpath = '/'.join(splitted_case_dir_path[:-2]) + '/' + splitted_case_dir_path[-2] + '--' + key_x + '-' + key_y + '.png'
+def update_json():
+    """
+    Update the content of cond.json
+    :return:
+    """
+def save_plot(root_directory, ax, key_x, key_y, key_c, im):
+    cbar = plt.colorbar(im)
+    cbar.set_label(key_c, rotation=270)
+    #ax.set_xlim([0.001, 0.1])
+    #ax.set_ylim([1, 1e6])
+    ax.set_xlabel(key_x)
+    ax.set_ylabel(key_y)
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.set_title(root_directory)
+    imgpath = root_directory + key_x + '-' + key_y + '.png'
+    #plt.show()
     plt.savefig(imgpath, transparent=True)
 
 
@@ -57,8 +91,4 @@ if __name__ == "__main__":
     # dissolCases_directory = 'C:/Users/tohoko.tj/dissolCases/seed7000-stdev0_15/'
     root_directory = 'C:/Users/tohoko.tj/dissolCases/'
 
-    dissolCases_directories = [name for name in os.listdir(root_directory) if os.path.isdir(root_directory + name)]
-    print(str(len(dissolCases_directories)) + ' batch detected.')
-    for dissolCases_directory in dissolCases_directories:
-        print('working on ' + dissolCases_directory + '...')
-        load_json_cond(root_directory + dissolCases_directory + '/')
+    load_json_cond(root_directory)
